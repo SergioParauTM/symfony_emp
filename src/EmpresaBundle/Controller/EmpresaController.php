6987d33e5f3e98ab7e7f06fe975eb2e0d11082f2
Sergio Parau <sergiopara@outlook.com>
2016-10-28 11:34:43 +0200

    Add new emp in bbdd

--- src/EmpresaBundle/Controller/EmpresaController.php
+++ src/EmpresaBundle/Controller/EmpresaController.php
@@ -2,7 +2,10 @@
 
 namespace EmpresaBundle\Controller;
 
+use EmpresaBundle\Entity\Empresa;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use EmpresaBundle\Form\EmpresaType;
+use Symfony\Component\HttpFoundation\Request;
 
 
 class EmpresaController extends Controller
@@ -14,6 +17,32 @@ class EmpresaController extends Controller
         // find *all* products
         $empresas = $repository->findAll();
 
-        return $this->render('EmpresaBundle:Empresa:all.html.twig',array('empresas'=>$empresas));
+        return $this->render('EmpresaBundle:Empresa:all.html.twig', array('empresas' => $empresas));
+    }
+
+    public function newAction(Request $request)
+    {
+        $newEmpresa = new Empresa();
+        $form = $this->createForm(EmpresaType::class, $newEmpresa); //generamos formulario en blanco
+        $form->handleRequest($request);
+
+        //En caso de que request sea enviado y valido
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $newEmpresa = $form->getData(); //guardamos los datos de la empresa
+
+            //Insertarlos dentro de la base de datos
+            $newEmp = $this ->getDoctrine()->getManager();
+
+            $newEmp-> persist($newEmpresa);
+
+            $newEmp ->flush();
+
+
+            return $this->redirectToRoute('all_empresa');
+        }
+
+        return $this->render('EmpresaBundle:Empresa:newEmp.html.twig', array('form' => $form->createView()));
     }
 }
