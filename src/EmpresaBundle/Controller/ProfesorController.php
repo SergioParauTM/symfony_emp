4a6789eaa9d96e3ce14872828838798d2274c901
Sergio Parau <sergiopara@outlook.com>
2016-11-21 11:57:15 +0100

    Añadimos formulario de añadir Profesores

--- src/EmpresaBundle/Controller/ProfesorController.php
+++ src/EmpresaBundle/Controller/ProfesorController.php
@@ -3,6 +3,8 @@
 namespace EmpresaBundle\Controller;
 
 use EmpresaBundle\Entity\Empresa;
+use EmpresaBundle\Entity\Profesor;
+use EmpresaBundle\Form\ProfesorType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use EmpresaBundle\Form\EmpresaType;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,6 +20,32 @@ class ProfesorController extends Controller
         $profesor = $repository->findAll();
 
         return $this->render('EmpresaBundle:Empresa:profesoresAll.html.twig', array('profesores' => $profesor));
+    }
+
+    public function newAction(Request $request)
+    {
+        $newProfesor = new Profesor();
+        $form = $this->createForm(ProfesorType::class, $newProfesor); //generamos formulario en blanco
+        $form->handleRequest($request);
+
+        //En caso de que request sea enviado y valido
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $newProfesor = $form->getData(); //guardamos los datos de la empresa
+
+            //Insertarlos dentro de la base de datos
+            $newPr = $this ->getDoctrine()->getManager();
+
+            $newPr-> persist($newProfesor);
+
+            $newPr ->flush();
+
+
+            return $this->redirectToRoute('all_profesores');
+        }
+
+        return $this->render('EmpresaBundle:Empresa:newProf.html.twig', array('form' => $form->createView()));
     }
 
 }
---
+++ src/EmpresaBundle/Form/ProfesorType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace EmpresaBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProfesorType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nombre')
+            ->add('apellidos')
+            ->add('departamento')
+            ->add('save', SubmitType::class)
+            ->add('delete',ResetType::class)
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'EmpresaBundle\Entity\Profesor'
+        ));
+    }
+}
